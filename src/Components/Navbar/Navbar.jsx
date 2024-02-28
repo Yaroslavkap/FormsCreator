812b9532532587f04fd33707cfe6373ac251fc86
Yaroslavkap <yaroslavkap1950@gmail.com.com>
2024-02-28 11:35:23 +0300

    commit n

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = function(props) {
             
             <li><Link className={cl.links} to='/my' >Мое</Link></li>
 
-            <li><Link className={cl.links} to='/form' >Форма</Link></li>
+            {/* <li><Link className={cl.links} to='/form' >Форма</Link></li> */}
             
             
         </ul>
--- src/Components/Pages/My.jsx
+++ src/Components/Pages/My.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+
 
 function My() {
+
+  const [forms, setForms] = useState([
+    { formName: "Form1",
+      disc: "disc1"
+    },
+    { formName: "Form2",
+      disc: "disc2"
+    },
+    { formName: "Form3",
+      disc: "disc3"
+    }
+  ])
   return (
     <div className='my_content'>
-      <h1>My</h1>
+      <div className='my_content_header'>
+        <h className='my_content_header_h'>Мои формы</h>
+      </div>
+      
+      <div className='my_content_main'>
+        <div className='my_content_main_form'>
+          {forms.map((form, i) =>
+            <div className='my_content_element'>
+              <h className='element_h'><Link className='element_h' to='/form' >{form.formName}</Link></h>
+              <p>{form.disc}</p>
+            </div>
+          )}
+        </div>
+
+        <div className='my_content_main_buttons'>
+            <button type='buttom' className='my_content_button'>Создать форму</button>
+            <button type='buttom' className='my_content_button'>Создать голосование</button>
+            <button type='buttom' className='my_content_button'>Создать тестирование</button>
+        </div>
+
+      </div>
       
     </div>
   )
